b049665f7d1610e2d1585c22984dbd7a3ce397ff
Bayzed-Meer <rsbayzed@gmail.com>
2024-02-01 13:43:42 +0600

    Audio feature added

--- src/app/radio-stations/radio-stations.component.ts
+++ src/app/radio-stations/radio-stations.component.ts
@@ -12,9 +12,12 @@ export class RadioStationsComponent implements OnInit {
   playingStatus: Map<string, boolean> = new Map();
   currentPlayingStation: string | null = null;
   currentPlayingStationInfo: any = null;
-  playPauseIcon: string = 'play_arrow'; // Added variable for play/pause icon
+  playPauseIcon: string = 'play_arrow';
   stations: any[] = [];
   filteredStations: any[] = [];
+  timerDuration: number | null = null;
+  isTimerActive: boolean = false;
+  timerInterval: any;
 
   constructor(
     private stationsService: StationsService,
@@ -49,10 +52,16 @@ export class RadioStationsComponent implements OnInit {
   }
 
   applyFilters(filters: any) {
-    if (!filters.country && !filters.language && !filters.name) {
+    if (
+      !filters.country &&
+      !filters.language &&
+      !filters.name &&
+      !filters.url
+    ) {
       this.filteredStations = this.stations;
       return;
     }
+
     this.filteredStations = this.stations.filter((station) => {
       const countryFilter =
         !filters.country ||
@@ -70,7 +79,12 @@ export class RadioStationsComponent implements OnInit {
       const nameFilter =
         !filters.name ||
         station.name.toLowerCase().includes(filters.name.toLowerCase());
-      return countryFilter && languageFilter && nameFilter;
+
+      const urlFilter =
+        !filters.url ||
+        station.url.toLowerCase().includes(filters.url.toLowerCase());
+
+      return countryFilter && languageFilter && nameFilter && urlFilter;
     });
   }
 
@@ -85,7 +99,8 @@ export class RadioStationsComponent implements OnInit {
     );
   }
 
-  togglePlayPause(stationUuid: string) {
+  togglePlayPause(station: any) {
+    const stationUuid = station.stationuuid;
     const currentStatus = this.playingStatus.get(stationUuid) || false;
 
     // Stop the currently playing station if any
@@ -94,7 +109,7 @@ export class RadioStationsComponent implements OnInit {
       this.currentPlayingStation !== stationUuid
     ) {
       this.playingStatus.set(this.currentPlayingStation, false);
-      // Add logic to stop the audio for the currentPlayingStation
+      this.pauseAudio();
     }
 
     this.playingStatus.set(stationUuid, !currentStatus);
@@ -108,13 +123,38 @@ export class RadioStationsComponent implements OnInit {
       // Add logic for play/pause functionality (e.g., audio playback control)
       if (this.playingStatus.get(stationUuid)) {
         // Start playing the station
+        this.playAudio(station.url_resolved);
       } else {
         // Pause the station
+        this.pauseAudio();
       }
 
       // Update the play/pause icon in the footer
       this.updatePlayPauseIcon();
     }
+  }
+
+  playAudio(url: string): void {
+    const audioElement = document.getElementById(
+      'audioElement'
+    ) as HTMLAudioElement;
+    if (audioElement) {
+      audioElement.src = url;
+      audioElement.play();
+    }
+  }
+
+  pauseAudio(): void {
+    const audioElement = document.getElementById(
+      'audioElement'
+    ) as HTMLAudioElement;
+    if (audioElement) {
+      console.log('Pausing audio...');
+      audioElement.pause();
+    } else {
+      console.log('Audio element not found!');
+    }
+    this.updatePlayPauseIcon(); // Ensure the icon is updated when pausing manually
   }
 
   updatePlayPauseIcon() {
@@ -129,5 +169,70 @@ export class RadioStationsComponent implements OnInit {
       this.playPauseIcon = 'play_arrow';
       this.currentPlayingStationInfo = null;
     }
+  }
+
+  openTimerDialog(): void {
+    const userInput = prompt('Enter timer duration (in seconds):');
+    const parsedDuration = userInput ? parseInt(userInput, 10) : null;
+
+    if (
+      parsedDuration !== null &&
+      !isNaN(parsedDuration) &&
+      parsedDuration > 0
+    ) {
+      this.timerDuration = parsedDuration;
+      this.startTimer();
+    } else {
+      // Display an alert to the user for invalid input
+      alert(
+        'Invalid input for timer duration. Please enter a valid positive number.'
+      );
+    }
+  }
+
+  startTimer(): void {
+    if (!this.isTimerActive && this.timerDuration !== null) {
+      this.isTimerActive = true;
+      this.timerInterval = setInterval(() => {
+        this.timerDuration = (this.timerDuration ?? 0) - 1;
+
+        if (this.timerDuration <= 0) {
+          this.stopTimer();
+        }
+      }, 1000);
+    }
+  }
+
+  stopTimer(): void {
+    clearInterval(this.timerInterval);
+    this.isTimerActive = false;
+    this.timerDuration = null;
+
+    // Pause the station when the timer finishes
+    this.pauseAudio();
+  }
+
+  audioEnded() {
+    this.stopTimer();
+
+    if (this.currentPlayingStation !== null) {
+      this.playingStatus.set(this.currentPlayingStation, false);
+      this.currentPlayingStation = null;
+      this.updatePlayPauseIcon();
+    }
+  }
+
+  formatTime(seconds: number | null): string {
+    if (seconds === null || isNaN(seconds) || seconds <= 0) {
+      return '00:00';
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
   }
 }
